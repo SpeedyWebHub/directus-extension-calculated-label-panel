903d70d534d7b7579fc7fec4dd0dcff9b3f3b8c7
adameus03 <amadeusz.sitnicki2@gmail.com>
2025-05-25 22:49:57 +0200

    refactor panel

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { definePanel } from '@directus/extensions-sdk';
-import PanelComponent from './panel.vue';
+import CalculatedPanelComponent from './calculated-panel.vue';
 
 export default definePanel({
 	id: 'panel-calculated',
 	name: 'Calculated Panel',
 	icon: 'box',
 	description: 'This is a flexible panel that can display calculated values.',
-	component: PanelComponent,
+	component: CalculatedPanelComponent,
 	options: [
 		{
 			field: 'field',
